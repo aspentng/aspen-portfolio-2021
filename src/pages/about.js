f7f8bb4012d2f74699d9b6da4c87acdf4f6dc700
Aspen Tng <aspentng@gmail.com>
2021-01-28 00:10:07 +0800

    add about page

---
+++ src/pages/about.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Helmet from "react-helmet";
+import { graphql } from "gatsby";
+import Layout from "../components/layout";
+
+const AboutPage = ({ data: { site } }) => {
+  return (
+    <Layout>
+      <Helmet>
+        <title>About — {site.siteMetadata.title}</title>
+        <meta
+          name="description"
+          content={"About page of " + site.siteMetadata.description}
+        />
+      </Helmet>
+      <div>
+        {site.siteMetadata.title}
+        {site.siteMetadata.about}
+      </div>
+    </Layout>
+  );
+};
+export default AboutPage;
+export const pageQuery = graphql`
+  query AboutPageQuery {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`;
