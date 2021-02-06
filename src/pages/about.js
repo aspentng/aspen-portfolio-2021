97d4b13e000c7d5ef0bc0780acea59211d80bc7d
Aspen Tng <aspentng@gmail.com>
2021-02-06 23:59:08 +0800

    stylistic changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Helmet from "react-helmet";
 import Layout from "../components/layout";
-import HeroHeader from "../components/heroHeader";
-
 import { StaticQuery, graphql } from "gatsby";
 import Img from "gatsby-image";
 
